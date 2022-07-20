0ba76c3bd746a8ae323dccb17d8f111390303dbd
maxthestranger <thestrangermax@gmail.com>
2022-07-20 11:17:59 +0300

    add post#index and post#show integration tests

--- spec/helpers/models_helper.rb
+++ spec/helpers/models_helper.rb
@@ -1,17 +1,17 @@
 def create_and_activate_user(name)
     user = User.create!(
-      name:, photo: "#{name}.jpg", bio: "Bio of #{name}"
+      name:, photo: "#{name}.jpg", bio: "Bio of #{name}", posts_counter: 0
     )
     # user validates and save
     user.save
     user
   end
   
-  def create_posts_for_user(author, count: 1)
+  def create_posts_for_user(user, count: 1)
     posts = []
     count.times do |i|
       posts << Post.create!(
-        author:,
+        user:,
         title: "Post #{i}",
         text: "This is body of post #{i}",
         comments_counter: 0,
@@ -21,11 +21,11 @@ def create_and_activate_user(name)
     posts
   end
   
-  def create_comments_for_post_by_user(post, author, count: 1)
+  def create_comments_for_post_by_user(post, user, count: 1)
     comments = []
     count.times do |i|
       comments << Comment.create!(
-        author:,
+        user:,
         post:,
         text: "Comment #{i}"
       )
