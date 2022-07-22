7b67722077a6f812570617bd8ac5a3ca8b0ea278
maxthestranger <thestrangermax@gmail.com>
2022-07-22 10:07:24 +0300

    generate controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+
+  get 'users/show'
+  get 'users/:user_id' => 'users#show', as: :user
+  get 'users/:user_id/posts' => 'posts/index', as: :user_posts
+  get 'users/:user_id/posts/:post_id' => 'posts#show', as: :user_posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "home#index"
 end
