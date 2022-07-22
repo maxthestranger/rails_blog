1e2e958abe0f5a866674d573af486360c0223dd0
maxthestranger <thestrangermax@gmail.com>
2022-07-22 08:25:54 +0300

    add model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :comment, dependent: :destroy
   has_many :like, dependent: :destroy
 
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def three_recent_posts
     post.includes(:user).order(created_at: :DESC).limit(3)
   end
