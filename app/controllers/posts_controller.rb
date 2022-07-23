609ebad4c494722a793fe4e113f409ce065f3ba8
maxthestranger <thestrangermax@gmail.com>
2022-07-23 10:06:46 +0300

    add views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.includes(:user).all
+    @posts = Post.includes(:user).where(user: params[:user_id])
   end
 
   def show
