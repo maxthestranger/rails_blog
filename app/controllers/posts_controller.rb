8b21c2b8f4c75b7f3427a96f9e8666159a66b121
maxthestranger <thestrangermax@gmail.com>
2022-07-23 10:58:54 +0300

    add create and new action for post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,26 @@ class PostsController < ApplicationController
     @posts = Post.includes(:user).where(user: params[:user_id])
   end
 
+  def create
+    @post = Post.new(params[:post].permit(:title, :text))
+    @post.user = current_user
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+
+    if @post.save
+      flash[:notice] = "Post created successfully!"
+      redirect_to user_post_path(current_user, @post)
+    else
+      flash[:error] = "Post creation failed!"
+      render :new
+    end
+  end
+
+  def new
+    @post = Post.new
+    render :new
+  end
+
   def show
     @post = Post.includes(:user).find(params[:id])
   end
